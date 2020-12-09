0f5d8f268a0de3dbae98bdcc3528926d4b9fb6a8
Daniel Njoroge Kinyanjui <dkinyanjui@safaricom.co.ke>
2020-12-09 15:37:41 +0300

    Text From File

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,15 +3,17 @@ import EncryptionLogic.Encrypt;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
 
     private static String[] alphabets = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V","W", "X", "Y", "Z"};
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File content = new File("src/main/resources/text.txt");
-        Scanner input = new Scanner(content);
+    public static void main(String[] args) throws IOException {
+        File rawText = new File("src/main/resources/text.txt");
+        Scanner input = new Scanner(rawText);
         String rawString = input.nextLine().toUpperCase();
 //        stringValidator(rawString);
         Encrypt encrypt = new Encrypt();
@@ -20,9 +22,25 @@ public class App {
         Decrypt decrypt = new Decrypt();
         decrypt.setKey(10);
         decrypt.setInput(encrypt.getOutput());
-        System.out.println("Input String: ".concat(rawString)
-                .concat("\n Encrypted String: ").concat(encrypt.getOutput())
-                .concat("\n Decrypted String: ").concat(decrypt.getOutput()));
+        String fileName = "src/main/resources/encoded.txt";
+        File f = new File(fileName);
+        if(f.exists() && !f.isDirectory()) {
+            writeToFile(rawString, encrypt, decrypt, fileName);
+        } else {
+            if (f.createNewFile())
+                writeToFile(rawString, encrypt, decrypt, fileName);
+        }
+    }
+
+    private static void writeToFile(String rawString, Encrypt encrypt, Decrypt decrypt, String fileName) throws IOException {
+        System.out.println("Please check results on this file: ");
+        FileWriter write = new FileWriter(fileName);
+        write.write("++++++++++++++++++++++++++++++++++++++++++++++++");
+        write.write("\nRaw Text: ".concat(rawString));
+        write.write("\nEncoded Text: ".concat(encrypt.getOutput()));
+        write.write("\nDecoded Text: ".concat(decrypt.getOutput()));
+        write.write("\n++++++++++++++++++++++++++++++++++++++++++++++++\n");
+        write.close();
     }
 
     private static void stringValidator(String text) throws RuntimeException{
