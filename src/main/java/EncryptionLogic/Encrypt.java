301f1f240392c74336b0125fee7fd3a63e044cc4
Daniel Njoroge Kinyanjui <dkinyanjui@safaricom.co.ke>
2020-12-09 12:51:38 +0300

    Update on Encrypt

--- src/main/java/EncryptionLogic/Encrypt.java
+++ src/main/java/EncryptionLogic/Encrypt.java
@@ -1,12 +1,20 @@
 package EncryptionLogic;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Encrypt {
     private String input;
     private String output;
-    private int key = 10;
+    private int key;
     private String[] alphabets = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V","W", "X", "Y", "Z"};
 
     public Encrypt() {
+    }
+
+    public Encrypt(int key) {
+        this.key = key;
     }
 
     public Encrypt(String input, String output) {
@@ -31,6 +39,14 @@ public class Encrypt {
         this.output = encryptString(input);
     }
 
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
     private String encryptString(String input){
         String phrase = input.toUpperCase();
         String output = "";
@@ -39,23 +55,33 @@ public class Encrypt {
             char[] inputSplit = phrase.toCharArray();
             for (char letter : inputSplit){
                 String theletter = String.valueOf(letter);
-                if (!theletter.equals(" ")) {
+                if (theletter.equals(" ") || !arrayToList(alphabets).contains(theletter)) {
+                    output = output + " ";
+                } else {
                     for (int i = 0; i <= alphabets.length; i++) {
                         if (theletter.equalsIgnoreCase(alphabets[i])) {
-                            index = i + key;
-                            if (index > alphabets.length)
-                                output = output + alphabets[index - 26];
+                            index = i + getKey();
+                            if (index >= alphabets.length)
+                                output = output + alphabets[index - alphabets.length];
                             else
                                 output = output + alphabets[index];
                             break;
                         }
                     }
-                } else
-                    output = output + " ";
+                }
             }
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
+        System.out.println("Out: ".concat(output));
         return output;
+    }
+
+    private List<String> arrayToList(String[] alphabets){
+        List<String> list = new ArrayList<>();
+        for (String letter : alphabets){
+            list.add(letter);
+        }
+        return list;
     }
 }
