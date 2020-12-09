2bdacd9df1b5a258b69373ffaf09e64a328d9e60
Daniel Njoroge Kinyanjui <dkinyanjui@safaricom.co.ke>
2020-12-09 12:53:02 +0300

    Update on Test

--- src/test/java/caesarCipherTest.java
+++ src/test/java/caesarCipherTest.java
@@ -1,3 +1,4 @@
+import EncryptionLogic.Decrypt;
 import EncryptionLogic.Encrypt;
 import org.junit.Test;
 
@@ -13,11 +14,32 @@ public class caesarCipherTest {
     }
 
     @Test
-    public void testEncryption_encryptionValidation_boolean(){
+    public void testEncryption_decryptingString_boolean(){
         Encrypt encrypt = new Encrypt();
-        String input = "My name is Daniel";
-        encrypt.setInput(input);
-        String encryptedString = "GS HUGY CM XUqHCYF";
+        encrypt.setKey(23);
+        encrypt.setInput("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG");
+        String encryptedString = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
         assertEquals(encryptedString, encrypt.getOutput());
+    }
+
+    @Test
+    public void testDecryption_encryptString_boolean(){
+        Decrypt decrypt = new Decrypt();
+        decrypt.setKey(20);
+        decrypt.setInput("GS HUGY CM XUHCYF");
+        String rawString = "My name is Daniel".toUpperCase();
+        assertEquals(rawString, decrypt.getOutput());
+    }
+
+    @Test
+    public void testBoth_encryptAndDecrypt_string(){
+        String rawString = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG".toUpperCase();
+        Encrypt encrypt = new Encrypt();
+        encrypt.setKey(23);
+        encrypt.setInput(rawString);
+        Decrypt decrypt = new Decrypt();
+        decrypt.setKey(23);
+        decrypt.setInput(encrypt.getOutput());
+        assertEquals(rawString, decrypt.getOutput());
     }
 }
